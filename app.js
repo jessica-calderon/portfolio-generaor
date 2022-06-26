ce2e53ec317097ed95e03e26e0a4f873183e9102
jessica-calderon <calderonjessica13@yahoo.com>
2022-06-25 22:19:26 -0500

    inquirer functionality

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
+/* const fs = require('fs');
 
 // reassign page-template.js module.exports object to generatePage variable in app.js
 const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2);
-
-const [name, github] = profileDataArgs;
+const pageHTML = generatePage (name, github);
 
 fs.writeFile('./index.html', generatePage(name, github), err => {
     if (err) throw new Error(err);
     
     console.log('Portfolio complete! Checkout index.html to see the output!');
-});
+}); */
